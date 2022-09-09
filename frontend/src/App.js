afa1f8577b45bd14546fc02095853a5486fb7368
Leo Niemi <niemi.leo@gmail.com>
2022-09-09 11:57:29 +0300

    Clean up code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,9 @@ import { useState, useEffect} from 'react'
 import fileService from './services/files'
 import Button from '@mui/material/Button';
 
-
 const App = () => {
-
   const [files, setFiles] = useState([])
+
   useEffect(() => {
     fileService.getAll().then(files =>
       setFiles( files )
@@ -15,14 +14,11 @@ const App = () => {
     fileService.getMidiData("Movie_Themes_-_Star_Wars_-_by_John_Willams.mid").then(data => {
       console.log(data)
     })
-    
   }, [])
+
   return (
     <div>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
       <Header/>
       <FileList uploadedFiles={files}></FileList>
       <Button variant="contained" onClick={()=> console.log("Naps")}>Demo of a button</Button>
--- frontend/src/components/FileList.js
+++ frontend/src/components/FileList.js
@@ -4,42 +4,36 @@ import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 import InboxIcon from '@mui/icons-material/Inbox';
 import DraftsIcon from '@mui/icons-material/Drafts';
 
 const FileList = ({uploadedFiles}) => {
-    
     if (uploadedFiles === null) {
         return (<div>
             <p>No files present.</p>
         </div>)
     } else {      
-            
-        
-    return (        
-    <div>
-        <link  rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
-        <Box>
-        {uploadedFiles.map(f => 
-            <List  key ={f.id}>
-          <Divider />
-            <ListItem disablePadding>
-            <ListItemButton onClick={()=> {console.log("This should generate music from", f.name)}}>
-                <ListItemText primary={f.name}/>
-            </ListItemButton>
-            </ListItem>
-            <Divider />
-        </List>
-
-        )}
-           
-        </Box>
-    </div>
-    );
-}}
-
+        return (        
+            <div>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+                <Box>
+                    {uploadedFiles.map(f => 
+                        <List  key ={f.id}>
+                            <Divider />
+                                <ListItem disablePadding>
+                                <ListItemButton onClick={()=> {console.log("This should generate music from", f.name)}}>
+                                <ListItemText primary={f.name}/>
+                                </ListItemButton>
+                                </ListItem>
+                            <Divider />
+                        </List>
+                    )}
+                </Box>
+            </div>
+        );
+    }
+}
 
 export default FileList
