a047f32072c1c0734ce52b64a763b307bb0fd309
Leo Niemi <niemi.leo@gmail.com>
2022-09-12 10:17:53 +0300

    Initial midi JSON parsing example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,37 @@ import Header from "./components/Header";
 import FileList from "./components/FileList";
 import { useState, useEffect} from 'react'
 import fileService from './services/files'
+import JSONmidiParser from "./components/JSONmidiParser";
 
 const App = () => {
 
   const [files, setFiles] = useState([])
+  const [midiAsJSON, setMidiAsJSON] = useState([])
+  const [isLoading, setLoading] = useState(true);
+
   useEffect(() => {
     fileService.getAll().then(files =>
-      setFiles( files )
+      setFiles( files ),
+      setLoading(false)
     )
     
     fileService.getMidiData("Movie_Themes_-_Star_Wars_-_by_John_Willams.mid").then(data => {
-      console.log(data)
+      setMidiAsJSON( data )
+      
     })
     
   }, [])
+
+  if (isLoading) {
+    return (
+      <h1>Loading...</h1>
+    )
+  }
   return (
     <div>
       <Header/>
       <FileList uploadedFiles={files}></FileList>
+      <JSONmidiParser midiData={midiAsJSON}/>
     </div>
   );
 }
