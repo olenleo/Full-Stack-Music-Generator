c272845fea4ec2611ebd9aff0e0c909547f1c70c
Leo Niemi <niemi.leo@gmail.com>
2022-09-19 11:34:06 +0300

    Add Tracklist.js component w/ initial material UI format

---
+++ frontend/src/components/TrackList.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+
+const TrackList = ( { midiDataAsJSON, handleClick }) => {
+	if (midiDataAsJSON === null || midiDataAsJSON === undefined || midiDataAsJSON.length === 0) {
+		return(
+			<div>
+				<h3>The track list should be here</h3>
+				
+			</div>
+		);
+	} else {
+		console.log('TrackList recieves ', midiDataAsJSON);
+		const trackInfo = midiDataAsJSON.track;
+		console.log('trackInfo', trackInfo);
+		let text = '';
+		return (        
+			<div>
+				<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+				<h3>Available MIDI tracks</h3>
+				<Box>
+					{trackInfo.map((track, index) => 
+						<List key ={index}>
+							<Divider />
+							<ListItem disablePadding>
+								<ListItemButton onClick={()=> {handleClick(index);}}>
+									{`Track ${index} | Events: ${track.event.length}`}
+									<ListItemText primary=''/>
+								</ListItemButton>
+							</ListItem>
+							<Divider />
+						</List>
+					)}
+				</Box>
+				
+			</div>
+		);
+	}
+};
+
+export default TrackList;
