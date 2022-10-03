53ec8c4fe0b879b5134f0cbc7ff098b697eea7d3
Leo Niemi <niemi.leo@gmail.com>
2022-10-03 12:02:50 +0300

    Intermediary commit: Fix noteStack erroneus splice

--- src/utils/NoteReader.js
+++ src/utils/NoteReader.js
@@ -1,11 +1,12 @@
+const { Trie } = require('./Trie');
 /* eslint-disable no-unused-vars */
 // Initialise variables
 let noteEvents = []; 
 const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C'];
 const tresholds = [109,97,85,73,61,49,37,25,13];
 let totalReadNotes = 0;
-let firstNoteOffset = 0;
 
+const trie = new Trie;
 const len = 5; // Length of trie 'word', or rather the length of saved note patterns. TODO: This should arrive from props.
 
 // DeltaTime and 'note on' & 'note off'-related variables
@@ -28,7 +29,6 @@ let freqArray = [];
 for (let i = 0; i < len; i++) {
 	freqArray.push('');
 }
-
 
 function NoteReader() {
 	//this.division = division
@@ -54,85 +54,61 @@ const parseOctave = (note) => {
 	}
 };
 
-//              Fix method below!
-// V --- V                      V---- V
-const formatAndPushNoteEvent = (event, index) => {
-	if (event.type === 9 || event.type === 11) {
-		const key = '' + event.deltaTime + index;
-		const pitch = parseNote(event.data[0]);
-		const noteJSONarray = {
-			note: `:${pitch}`,
-			amp: `${event.data[1]}`,
-			sleep: 'sleep 1',
-			key: `${key}`
-		};
-		noteEvents.push(noteJSONarray);
-	}
-};
-
 const setUpMidiData = (trackdata) => {
-	console.log('Then its:', trackdata);
+	noteStack = [];
+	trackdata.event.map((event) => {
+		if (event.type == 9 || event.type == 8){
+			noteEvents.push(event);
+		}
+	}
+	);
 	trackdata.event.sort((a,b) => {
 		return a.deltaTime - b.deltaTime;
 	});
-
-	trackdata.event.map((event, index) => (
-		formatAndPushNoteEvent(event, index)
-	));
-	let result = [];
-	for (let object in noteEvents) {
-		result.push(noteEvents[object]);
-	}
-
-	console.log('Finally it is ', result, ' type ', typeof result);
-	noteEvents = result;
+	console.log('Complete', noteEvents); 
 };
 
 NoteReader.prototype.readJSON = function(midiAsJSON, selectedTrack) {
-	console.log('Dis right here:', midiAsJSON);
-	console.log('Track', midiAsJSON.track[selectedTrack]);
-	const first = midiAsJSON.track[selectedTrack];
-	console.log('first its', first);
+	console.log('NoteStack:', noteStack);
 	setUpMidiData(midiAsJSON.track[selectedTrack]);
-	this.firstNoteOffset = getOffset() + 1;
 	for (let i = 0; i < noteEvents.length; i++) {
-        console.log(i);
 		handleNote(noteEvents[i], previousNoteEndDeltatime);
 	}
-	console.log('Read done. Offset was', firstNoteOffset);
-	
 };
 
 function handleNote(note, lastNoteEnd) {
-	absoluteTime = note.deltaTime - firstNoteOffset + 1;
+	absoluteTime = note.deltaTime;
 	const pitch = note.data[0];
 	const amp = note.data[1];
 	// A note is ending:
 	if (noteOperationIsEnd(note)) {
+        
 		if (absoluteTime > 0) {
-			console.log('handleNote @ deltatime:', absoluteTime);
+			//
 		}
 		// Determine if parser encounters a break, a chord or a new note
-		if (lastSeenNoteEnd == absoluteTime) {
+		if (lastSeenNoteEnd == absoluteTime - 1) {
+			//console.log('end type 1  for ', pitch);
 			// No notes were running: The last seen note end equals current, absolute time.
 			//console.log('END 1:',pitch, amp, absoluteTime - noteStartTimes[pitch], 0)
-			// insert(pitch, amp, duration, rest, trie, noteStack)
-			insertToStack(pitch, previousNoteAmplitude, absoluteTime - noteStartDeltaTimes[pitch], 0);
+			// insert(pitch, amp, duration, rest, trie)
+			insertToStack(note);
 		} else {
+			//console.log('end type 2 for ', pitch);
 			// A note was running; this is a chord!
 			noteStartDeltaTimes.splice(parseInt(pitch), 0, 0);
 			// insert(pitch, amp, duration, rest, trie)
-			insertToStack(pitch, previousNoteAmplitude, absoluteTime - noteStartDeltaTimes[pitch], absoluteTime - lastNoteEnd);
+			insertToStack(note);
 			previousNoteEndDeltatime = absoluteTime;
 		}
 		// A note is starting: 
 	} else if (noteOperationIsStart) {  
-
 		noteStartDeltaTimes.splice(parseInt(pitch), 0, absoluteTime);
 		previousNoteAmplitude = amp;
-		this.lastSeenNoteStart = absoluteTime;
+		lastSeenNoteStart = absoluteTime;
+		//console.log('New note [', pitch, ']', noteStartDeltaTimes[parseInt(pitch)], 'start at ', absoluteTime);
 	}
-	return this.lastSeenNoteEnd;
+	return lastSeenNoteEnd;
 }
 /**
  * Insert operation
@@ -141,27 +117,19 @@ function handleNote(note, lastNoteEnd) {
  * @param {*} duration: note duration in midi time units, integer > 1. Duration = 0 -> chords
  * @param {*} rest: this is a break, duration in midi time unitis, integer > 1 
  */
-function insertToStack(pitch, amp, duration, rest) {
+function insertToStack(note) {
 	if (noteStack.length < len) {
-		if (rest === null) { rest = 0; }
-		const contentAsJSON = JSON.parse(
-			`{
-                "pitch" : ${pitch}, 
-                "amp": ${amp}, 
-                "duration": ${duration},
-                "rest": ${rest},
-                "freq": 1,
-                "children": []
-            }`);
-		noteStack.push(contentAsJSON);
+		noteStack.push(note);
 	} else {
-		console.log('Full stack: ', noteStack);
-		noteStack.shift();
+		const [first, ...rest] = noteStack;
+		console.log('First, rest: ', first, ' --- ', rest);
+		noteStack = rest;
 	}
 }
 
 // ! note_on with amplitude 0 is a note_off !
 function noteOperationIsStart(note) {
+	
 	return note.type == 9 && note.data[1] > 0;
 }
 // Note off commands are expressed by:
@@ -170,13 +138,6 @@ function noteOperationIsStart(note) {
 // Note x with amplitude 0
 function noteOperationIsEnd(note) {
 	return note.type == 8 || (note.data[1] == 0);
-}
-
-// First note_on might start after deltatime
-function getOffset() {
-	for (let i of noteEvents) {
-		return i.deltaTime;
-	}
 }
 
 export default NoteReader;
